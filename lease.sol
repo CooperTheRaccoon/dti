09350fbbd75dd82d61c54507a520894961f0e3cc
CooperTheRaccoon <daniel-angelo@sapo.pt>
2021-05-01 19:51:58 +0100

    CRL!

--- lease.sol
+++ lease.sol
@@ -16,6 +16,8 @@ contract Lease {
     uint residualValue;
     uint32 numPaidMonthlyInstallments;
     uint startTime;
+    uint numCycles;
+    uint lastNumCycle;
     address payable lessor; 
     address payable lessee; 
     address payable insuranceCompany;
@@ -36,12 +38,13 @@ contract Lease {
         _;
     }
     
-    modifier hasPassed3Minutes() {
-        require (block.timestamp >= startTime + 30 seconds);
-        startTime = block.timestamp;
-        _;
-    }
-    
+    //modifier hasPassed3Minutes() {
+    //    require (block.timestamp >= startTime + 30 seconds);
+    //    startTime = block.timestamp;
+    //    numCycles++;
+    //    _;
+    //}
+
     constructor() public {
         
         state = State.INIT; //estado lógico inicial
@@ -54,6 +57,8 @@ contract Lease {
         smartContract = payable(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB);
         
         numPaidMonthlyInstallments = 0;
+        numCycles = 1;
+        lastNumCycle = 0;
         startTime = 0;
 
     }
@@ -106,16 +111,28 @@ contract Lease {
         return true;
     }
     
-    function payRental() inState(State.VALID) hasPassed3Minutes() payable public returns (bool) {
+    
+    function advanceCycle() inState(State.VALID) public returns (bool) {
+        require(block.timestamp >= startTime + 45 seconds);
+        startTime = block.timestamp;
+        numCycles++;
+        return true;
+    }
+
+    
+    function payRental() inState(State.VALID) payable public returns (bool) {
         
         //vai ter que verificar se pode ser chamado
         require(payable(msg.sender) == lessee);
         require(balances[lessee] >= rental);
+        require(numCycles > lastNumCycle && numCycles <= duration);
+        
         balances[lessee] -= monthlyInstallment;
         balances[smartContract] += monthlyInstallment;
         balances[lessee] -= monthlyInsurance;
         balances[insuranceCompany] += monthlyInsurance;
-        paidRentals[numPaidMonthlyInstallments] = true;
+        paidRentals[numCycles] = true;
+        lastNumCycle = numCycles;
         numPaidMonthlyInstallments++;
         return true;
     }
@@ -128,6 +145,7 @@ contract Lease {
         require(balances[smartContract] >= amount);
         balances[smartContract] -= amount;
         balances[lessor] += amount;
+        return true;
     }
     
     function amortizeResidualValue() inState(State.VALID) payable public returns (bool) {
@@ -138,6 +156,7 @@ contract Lease {
         balances[lessee] -= msg.value;
         //atualiza-se
         setNewResidualValue(msg.value);
+        return true;
     }
     
     function liquidateLease() inState(State.VALID) payable public returns (bool) {
@@ -148,6 +167,7 @@ contract Lease {
         balances[lessee] -= monthlyInstallment * (duration - numPaidMonthlyInstallments);
         balances[smartContract] += monthlyInstallment * (duration - numPaidMonthlyInstallments);
         numPaidMonthlyInstallments = duration;
+        return true;
     }
     
     function addTokens(address entity) payable public{
@@ -173,6 +193,10 @@ contract Lease {
     
     function getNumPaidMonthlyInstallments() public view returns (uint) {
         return numPaidMonthlyInstallments;
+    }
+    
+    function getNumCycles() public view returns (uint) {
+        return numCycles;
     }
     
     function setLessorInput(uint32 a, uint32 b, uint32 c, uint32 d, uint32 e, uint32 f) private {
@@ -216,7 +240,7 @@ contract Lease {
         startTime = block.timestamp;
     }
     
-    function secondsRemaining() public view returns (uint256) {
+    function getSecondsPast() public view returns (uint) {
         return block.timestamp - startTime;
     }
 
@@ -224,5 +248,6 @@ contract Lease {
     //0,1000,5,2,10,20 (lessor)
     //5 (insurer)
     //2 (lessee)
+    //0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db (lessee)
     
 }
